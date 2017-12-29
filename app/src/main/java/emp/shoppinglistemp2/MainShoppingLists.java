d2fbbe99c5bab26056096b6ad1635c5be15af4a9
jeonshon <jean.marinsek@outlook.com>
2017-12-29 17:43:32 +0100

    my second commit

--- app/src/main/java/emp/shoppinglistemp2/MainShoppingLists.java
+++ app/src/main/java/emp/shoppinglistemp2/MainShoppingLists.java
@@ -1,8 +1,11 @@
 package emp.shoppinglistemp2;
 
+import android.widget.TextView;
+
 /**
  * Created by jeanm on 29/12/2017.
  */
 
 public class MainShoppingLists {
+    private TextView mTextMessage;
 }
